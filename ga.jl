c0a33abfb6211383743599f863d442e169322a92
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-01-20 16:46:36 +0100

    crossover

--- ga.jl
+++ ga.jl
@@ -1,20 +1,63 @@
 #Genetic Algorithm
+include("s2.jl")
 
-
-function select()
+function select(fit, K)
     
 end
 
-function crossover()
+function crossover_mutation(stocks, P, lambda, correlationMatrix, meanStandardA)
+    size = 1/P
+    lambdas = collect(0.01:size:1)
+    ret = 0.0
+    risk = 0.0
+    E = 1000.0
+
+    for l in lambdas
+        tempRet = 0.0
+        tempRisk = 0.0
+        tempE = 0.0
+        for i in 1:size(stocks, 1)
+            sumRisk = 0
+           for j in  1:size(stocks, 1)
+                V = correlationMatrix[i,j]*meanStandardA[i,2]*meanStandardA[j,2]
+                sumRisk+= stock[i, 2] * V * stock[j, 2]
+           end
+           tempRisk+=sumRisk
+
+           tempRet += stock[i, 2] * meanStandardA[i,1]
+
+        end
+
+        tempE = l*tempRisk - (1-l)*tempRet
+
+        if tempE < E
+            E = tempE
+            risk = tempRisk
+            ret = tempRet
+            lambda = l
+        end
+    end
     
 end
 
-function mutation()
-    
-end
 
-function genteticAlgorithm()
+function genteticAlgorithm(population, K, P, correlationMatrix, meanStandardA)
+    num_gen = 10
+
+    lambda = 0.01
+    ret = 0.0
+    risk = 0.0
+    E = 0.0
+
+    fitness = swarm_particle(population)
     
+    for gen in 1:num_gen
+        p_next = Array{Float64}(undef, 0, 2)
+        for pair in 1:size(fitness, 1)
+            stocks = select(fitness, K)
+            lambda, ret, risk, E = crossover_mutation(stocks, P, lambda, correlationMatrix, meanStandardA)
+        end
+    end
 end
 
 
