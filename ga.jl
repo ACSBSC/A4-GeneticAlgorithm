1129ed3c8bab8cd8b248653a1249414fa7fa27ac
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-01-21 11:58:45 +0100

    construct a better approach to GA

--- ga.jl
+++ ga.jl
@@ -1,63 +1,66 @@
 #Genetic Algorithm
+
+# Size of stocks
+# 𝜇, 𝜎  --> meanStandardA
+# p --> correlationMatrix
+# Vij = pij*𝜎i*𝜎j
+# L investment
+# U investment
+# L<=xi<=U 
+# P --> where 𝜆 comes from
+
+
 include("s2.jl")
 
-function select(fit, K)
-    
-end
+#tournament selection
+# 𝜇
+# output [index] 
+function selection(population, correlationMatrix) 
+    stocks = Array{Int64}(undef, 0, 1)
+    for i in 1:5
 
-function crossover_mutation(stocks, P, lambda, correlationMatrix, meanStandardA)
-    size = 1/P
-    lambdas = collect(0.01:size:1)
-    ret = 0.0
-    risk = 0.0
-    E = 1000.0
-
-    for l in lambdas
-        tempRet = 0.0
-        tempRisk = 0.0
-        tempE = 0.0
-        for i in 1:size(stocks, 1)
-            sumRisk = 0
-           for j in  1:size(stocks, 1)
-                V = correlationMatrix[i,j]*meanStandardA[i,2]*meanStandardA[j,2]
-                sumRisk+= stock[i, 2] * V * stock[j, 2]
-           end
-           tempRisk+=sumRisk
-
-           tempRet += stock[i, 2] * meanStandardA[i,1]
-
-        end
-
-        tempE = l*tempRisk - (1-l)*tempRet
-
-        if tempE < E
-            E = tempE
-            risk = tempRisk
-            ret = tempRet
-            lambda = l
-        end
+        stocks = [stocks; winner]
     end
+    return stocks
+end
+
+# one point crossover, change 𝜇 values between the selected stocks
+function crossover(selected, correlationMatrix)
     
+    for i in 1:5
+    
+    end
+    return correlationMatrix
+end
+
+function mutation()
+
 end
 
 
-function genteticAlgorithm(population, K, P, correlationMatrix, meanStandardA)
+
+function genteticAlgorithm(N)
     num_gen = 10
-
-    lambda = 0.01
-    ret = 0.0
-    risk = 0.0
-    E = 0.0
-
-    fitness = swarm_particle(population)
-    
+    pareto = false
+    population = collect(1:N)
+    𝜆 = 0.01
     for gen in 1:num_gen
-        p_next = Array{Float64}(undef, 0, 2)
-        for pair in 1:size(fitness, 1)
-            stocks = select(fitness, K)
-            lambda, ret, risk, E = crossover_mutation(stocks, P, lambda, correlationMatrix, meanStandardA)
+        p_next = Array{Float64}(undef, 0, 0)
+        for pair in 1:size(fitness, 1)/5
+            
+            selected = selection(population, correlationMatrix)
+            correlationMatrix = crossover(selected, correlationMatrix)
+            selected = mutation()
+
+            p_next = [p_next, selected] #array of indexes
+
         end
+
     end
+
+    #plot risk vs return scatter plot
+
+
 end
 
 
